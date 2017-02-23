4f2c4e9377dcb9cc85b38b00653075524dbe26ac
Gary Shirk <garyshirk@yahoo.com>
2017-02-22 18:13:37 -0600

    updates to admin site, initial work on views and url patterns

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from posts.models import Post
 
-admin.site.register(Post)
+# Create a modeladmin so that we can show timestamp via modeladmin list_display option in admin server
+class PostModelAdmin(admin.ModelAdmin):
+	#list_display = ["__unicode__", "timestamp", "updated", "content"]
+	list_display = ["title", "timestamp", "updated", "content"]
+	list_display_links = ["updated"]
+	list_filter = ["updated", "timestamp"]
+	search_fields = ["title", "content"]
+	list_editable = ["title"]
+	class Meta:
+		model = Post
+		# notice you include PostAdmin as an arg in the register method below
+
+admin.site.register(Post, PostModelAdmin)
+
+
